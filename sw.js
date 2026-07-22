15aa56c11040e3e17a3bf8d3b05015fa5a4fb1c5
Cl1ck-t0 <2187278031@qq.com>
2026-07-22 17:10:18 +0800

    Site updated: 2026-07-22 17:10:17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1784690107601";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1784711409608";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
